1379907e30d8e06b07e1a336bf0080b7346abdff
marius.crisan <X9493276A@csic.es>
2023-06-01 12:52:45 +0200

    Removed public test function

--- firmware/Sources/Test/HAL/hal_gpio_test.h
+++ firmware/Sources/Test/HAL/hal_gpio_test.h
@@ -11,17 +11,6 @@
 /*                    Declaration of local function prototypes                    */
 /**********************************************************************************/
 
-/**
- * @fn void TestPins(HAL_GPIO_output_e pinO, HAL_GPIO_input_e pinI, HAL_GPIO_pin_value_e value)
- * @brief Writes the value given in the output pin and verifies that the input pin
- * reads the same value as the one writen, using the functions developed.
- * @param pinO GPIO output pin to set.
- * @param pinI GPIO output pin to get.
- * @param value GPIO pin value to write and read.
- * @return @ref HAL_GPIO_RESULT_SUCCESS if initialize correctly,
- * @ref HAL_GPIO_RESULT_ERROR otherwise.
- */
-HAL_GPIO_result_e TestPins(HAL_GPIO_output_e pinO, HAL_GPIO_input_e pinI, HAL_GPIO_pin_value_e value);
 /**********************************************************************************/
 /*                    	 Declaration of exported functions                  	  */
 /**********************************************************************************/
