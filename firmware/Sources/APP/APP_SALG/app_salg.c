abc7cc366db628fc20924b9acd98dd1407db0164
Roberto Aldea <raldea@icb.csic.es>
2023-07-10 09:36:30 +0200

    Update SALG to make it work with all the code, improvement in code and added comments

--- firmware/Sources/APP/APP_SALG/app_salg.c
+++ firmware/Sources/APP/APP_SALG/app_salg.c
@@ -28,7 +28,8 @@ extern const MID_REG_limit_s EPC_CONF_limit_range;
 /**********************************************************************************/
 /*                     Definition of local symbolic constants                     */
 /**********************************************************************************/
-
+#define HYPERPERIOD 10
+#define HYPERLEDS 1500
 /**********************************************************************************/
 /*                    Definition of local function like macros                    */
 /**********************************************************************************/
@@ -41,6 +42,7 @@ extern const MID_REG_limit_s EPC_CONF_limit_range;
 /*                         Definition of local variables                          */
 /**********************************************************************************/
 volatile uint8_t rt_n_ints = 0, rt_int_triggered = 0, pw_meas_n_ints = 0;
+volatile uint16_t led_rt_n_ints = 0;
 
 MID_REG_limit_s limit = { 0 };
 MID_REG_control_s consign = {
@@ -106,10 +108,11 @@ APP_SALG_result_e InitSalgLimitRegister () {
 /*                        Definition of exported functions                        */
 /**********************************************************************************/
 #ifndef EPC_CONF_TESTING
-
+#include "main.h"
 void HAL_TMR_RT_Callback(){
 //	HAL_SysResume();
 	rt_n_ints += 1;
+	led_rt_n_ints +=1;
 	rt_int_triggered = 1;
 }
 
@@ -130,6 +133,7 @@ APP_SALG_result_e APP_SalgInit(){
 		// Set error status
 		errorStatus.intErr = MID_REG_ERROR_RAISED;
 		errorStatus.lastErrVal = (uint16_t) sysRes;
+		control.mode = MID_REG_MODE_ERROR;
 
 		// Turn of leds with error status
 		MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
@@ -139,17 +143,16 @@ APP_SALG_result_e APP_SalgInit(){
 			// There is not error on critical system or CAN, so communicate error through can network
 			APP_IfaceProcessPeriodic(&measures, &errorStatus);
 		}
-		// Disable control
-		// TODO: disable ctrl
+		APP_SalgIntError();
 
 		// Enter uC on sleep mode
-		HAL_SysPwrMode(HAL_SYS_MODE_SLEEP);
+//		HAL_SysPwrMode(HAL_SYS_MODE_SLEEP);
 	}
 
 
 	// Initialize registers
 	InitSalgLimitRegister(); //TODO: this returns allways APP_SALG_RESULT_SUCCESS. cast to ignore or do something :)
-	AppIfacePeriodicRegister();
+	AppIfaceInit();
 
 	#ifdef EPC_CONF_WDG_ENABLED
 		HAL_WdgInit();
@@ -161,14 +164,28 @@ APP_SALG_result_e APP_SalgInit(){
 APP_SALG_result_e APP_SalgStatusMachine(){
 	APP_SALG_result_e res = APP_SALG_RESULT_SUCCESS;
 	APP_CTRL_result_e ctrl_res = APP_CTRL_RESULT_SUCCESS;
-//	APP_IFACE_result_e iface_res = APP_IFACE_RESULT_SUCCESS;
+	APP_IFACE_result_e iface_res = APP_IFACE_RESULT_SUCCESS;
 
 	HAL_TmrStart(HAL_TMR_CLOCK_RT);
 	HAL_TmrStart(HAL_TMR_CLOCK_PWR_MEAS);
 	// Wait 2 cycles in order to have measurements in DMA
 	rt_n_ints = 0;
 	while(rt_n_ints < 2);
+	//Reset number of cycles
+	rt_n_ints = 0;
+	led_rt_n_ints = 0;
+//	while(1){
+//		while(rt_int_triggered == 0);
+//		rt_int_triggered = 0;
+//		rt_n_ints = rt_n_ints % HYPERPERIOD;
+//		if (rt_n_ints == 3){
+////			HAL_GPIO_TogglePin(Led3_GPIO_Port, Led3_Pin);
+//			res = MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
+//		}
+//	}
+
 	while(1){
+
 
 		// 9.0 Enters on sleep mode
 		/** CPU is stopped and all peripherals continue to operate.
@@ -178,39 +195,37 @@ APP_SALG_result_e APP_SalgStatusMachine(){
 //			HAL_SysPwrMode(HAL_SYS_MODE_SLEEP);
 		}
 
-
 		// 1.0 Timer interrupt raised, sampling temperature measures
+		rt_n_ints = rt_n_ints % HYPERPERIOD;
+		led_rt_n_ints = led_rt_n_ints % HYPERLEDS;
 		rt_int_triggered = 0;
 		pw_meas_n_ints = 0;
 
 		// TODO: check return values
 
 		// 2.0 Get measures
-		res |= MID_DabsUpdateMeas(MID_DABS_MEAS_ELECTRIC, &measures);
-		res |= MID_DabsUpdateMeas(MID_DABS_MEAS_TEMP, &measures);
-
-		// 3.0 Read for incoming messages
-//		res |= APP_IfaceIncommingMsg(&control, &measures, &errorStatus, &limit, &consign);
-
-		// 4.0 Apply control actions
+		res = MID_DabsUpdateMeas(MID_DABS_MEAS_ELECTRIC, &measures);
+		res = MID_DabsUpdateMeas(MID_DABS_MEAS_TEMP, &measures);
+		//If Measurements go wrong
+		// 3.0 Apply control actions
 		ctrl_res = APP_CtrlCheckErrors (&errorStatus, &measures, &limit);
-		if (ctrl_res != APP_CTRL_RESULT_SUCCESS){
+		if (ctrl_res == APP_CTRL_RESULT_ERROR_RAISED && control.mode!=MID_REG_MODE_ERROR){
 			// If error, set error mode and disable output.
 			consign.outStatus = MID_REG_DISABLED; 	// outStatus
-			consign.mode = MID_REG_MODE_ERROR; 	// mode
+			consign.mode = MID_REG_MODE_ERROR; 		// mode
 			consign.limitType = MID_REG_LIMIT_TIME; // limitType
 			consign.modeRef = 0;					// modeRef
-			consign.limRef = 0;					// limRef
+			consign.limRef = 0;						// limRef
 			ctrl_res = APP_CtrlApplyNewMode (&consign, &control, &measures);
 		}else if (ctrl_res == APP_CTRL_RESULT_SUCCESS && control.mode==MID_REG_MODE_ERROR){
 			//If last mode was error, and have been solved enter idle mode.
 			consign.outStatus = MID_REG_DISABLED; 	// outStatus
-			consign.mode = MID_REG_MODE_IDLE; 	// mode
+			consign.mode = MID_REG_MODE_IDLE; 		// mode
 			consign.limitType = MID_REG_LIMIT_TIME; // limitType
 			consign.modeRef = 0;					// modeRef
-			consign.limRef = 0;					// limRef
+			consign.limRef = 0;						// limRef
 			ctrl_res = APP_CtrlApplyNewMode (&consign, &control, &measures);
-		}else{
+		}else if (ctrl_res == APP_CTRL_RESULT_SUCCESS && control.mode!=MID_REG_MODE_ERROR){
 			//Check if changes in consign have been made, every change will disable and enable output
 			if (consign.limRef != control.limRef || consign.limitType != control.limitType ||
 					consign.mode != control.mode || consign.modeRef != control.modeRef ||
@@ -220,24 +235,56 @@ APP_SALG_result_e APP_SalgStatusMachine(){
 			//Update control
 			if (ctrl_res == APP_CTRL_RESULT_SUCCESS){
 				ctrl_res = APP_CtrlUpdate(&control, &measures, &limit);
+				memcpy(&consign, &control , sizeof(MID_REG_control_s));
 			}
 		}
 
+		//The following actions have to be executed just once every 10ms
+		// 4.0 Read for incoming messages
+		if (rt_n_ints == 0 || rt_n_ints == 1){
+			iface_res = APP_IfaceIncommingMsg(&control, &measures, &errorStatus, &limit, &consign);
+		}
 
 
 		// 5.0 Process periodic outgoing communication
-//		res |= APP_IfaceProcessPeriodic(&measures, &errorStatus);
+		else if (rt_n_ints == 2){
+			iface_res = APP_IfaceProcessPeriodic(&measures, &errorStatus);
+		}
 
 		// 6.0 Update LEDs
-		res |= MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
+		else if (led_rt_n_ints == 3){
+			res = MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
+		}
 
 		// 7.0 Notify user of status change
-//		res |= APP_IfaceNotifyModeChange(&control);
+		else if (rt_n_ints == 4){
+			iface_res = APP_IfaceNotifyModeChange(&control);
+		}
 
 		// 8.0 Heartbeat to whatchdog
 		#ifdef EPC_CONF_WDG_ENABLED
 			HAL_WdgRefresh();
 		#endif
-
+		if (res == APP_SALG_RESULT_ERROR || ctrl_res == APP_CTRL_RESULT_ERROR_INT || iface_res == APP_IFACE_RESULT_ERROR){
+			APP_SalgIntError();
+		}
 	} // End while
+}
+
+
+void APP_SalgIntError(){
+	errorStatus.intErr = MID_REG_ERROR_RAISED;
+	MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
+	//If Internal error, set error mode with disable output
+	consign.outStatus = MID_REG_DISABLED; 	// outStatus
+	consign.mode = MID_REG_MODE_ERROR; 		// mode
+	consign.limitType = MID_REG_LIMIT_TIME; // limitType
+	consign.modeRef = 0;					// modeRef
+	consign.limRef = 0;						// limRef
+	APP_CtrlApplyNewMode (&consign, &control, &measures);
+	APP_IfaceProcessPeriodic(&measures, &errorStatus);
+	while(1){
+		MID_DabsUpdateLeds(control.mode, measures.lsCurr, &errorStatus);
+		HAL_WdgRefresh();
+	}
 }
