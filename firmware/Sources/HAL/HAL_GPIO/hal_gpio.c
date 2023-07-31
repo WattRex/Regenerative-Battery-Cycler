9d4f7cee0f278e4c9153deb22e6ef7ff49622cfb
Pablo <pablo.pastor@csic.es>
2023-07-31 12:08:23 +0200

    LICENSE comment added

--- firmware/Sources/HAL/HAL_GPIO/hal_gpio.c
+++ firmware/Sources/HAL/HAL_GPIO/hal_gpio.c
@@ -1,6 +1,31 @@
 /*********************************************************************************
 * @file           : hal_gpio.c
-* @brief          : Implementation of HAL GPIO
+* @brief          : Implementation of Hardware Abstraction Layer (HAL) for GPIO 
+*	(General Purpose Input/Output). Provides functions for GPIO initialization, 
+*	setting output pins, and reading input pins. Includes configurations for 
+*	available input and output GPIO pins.
+***********************************************************************************/
+
+/*********************************************************************************
+*	
+*	Copyright (C) 2023  WattRex <https://github.com/WattRex/>
+*
+*	This program  is free software: you  can redistribute it and/or modify it under
+*	the terms of the GNU Affero General  Public  License  as published  by the Free
+*	Software  Foundation, either version 3 of the License, or (at  your option) any
+*	later  version.  This program  is   distributed  in  the hope that  it  will be
+*	useful,but   WITHOUT  ANY  WARRANTY;   without  even  the implied   warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General
+*	Public License for more details. You should   have  received a copy  of the GNU
+*	Affero   General  Public License  along   with  this  program.     If  not, see
+*	<https://www.gnu.org/licenses/>.
+*	
+*	This file is part of PowerTower, Regenerative Battery Cycler in
+*	<https://github.com/WattRex/Regenerative-Battery-Cycler/>
+*
+*@author           : WattRex <https://github.com/WattRex/>
+*@date			   : 2023-07-04
+*@version		   : v1.0.0
 ***********************************************************************************/
 
 /**********************************************************************************/
